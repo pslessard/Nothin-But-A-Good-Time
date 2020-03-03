169e22a61ef09b14775b8da0594b61a6e0df0c06
Petra Kumi <pkumi@wpi.edu>
2020-03-03 12:41:35 -0500

    upload box, csv and json file reading functionality that is reusable

--- templates/static/onload.js
+++ templates/static/onload.js
@@ -1,40 +1,50 @@
 'use strict';
 
-function new_upload(obj) {
+//gets called when new upload happens
+function new_upload_temp(obj) {
+
     console.log('started new upload');
 
     // console.log(obj.id)
     console.log(obj.files);
     let files = obj.files; // FileList object
     let name_list = [];
+    let data_dict_arr = [];
+
     for (let i = 0, f; f = files[i]; i++) {
         let reader = new FileReader();
-        var extension = f.name.split('.').pop().toLowerCase();//file extension from input file
-        if(extension==='csv'){
-            reader.onload = e => {read_csv(e, f.name);};
-            reader.readAsText(f);
-        }else if (extension==='json'){
-            reader.onload = e => {read_json(e, f.name);}
-        }
         reader.onerror = error_handler;
         name_list.push(f.name);
+        var extension = f.name.split('.').pop().toLowerCase();//file extension from input file
+        reader.onload = e => {
+            if (extension === 'csv') {
+                data_dict_arr = read_csv(e, f.name)
+            }
+            else if (extension === 'json') {
+                data_dict_arr = read_json(e, f.name)
+            }
+
+            //this will get the data as an array of dicts with x and y values nicely organized
+            post_process_data(data_dict_arr, name_list)
+        };
+        reader.readAsText(f)
 
     }
-    let nextSibling = obj.nextElementSibling;
-    nextSibling.innerText = name_list;
 }
 
 
-//csv file handling functions
-
 //handles what happens on json file load
-function read_json(event, filename){
-    console.log(JSON.parse(event.target.result));
-    return JSON.parse(event.target.result);
+function read_json(event, filename) {
+    let data_dict_arr = [];
+    data_dict_arr.push(JSON.parse(event.target.result))
+    return data_dict_arr
 }
 
 //handles what happens on csv file load
 function read_csv(event, filename) {
+    // let event = e;
+    //              let filename = f.name;
+    let data_dict_arr = []
     let csv = event.target.result;
     // console.log(event.target);
     let allTextLines = csv.split(/\r\n|\n/);
@@ -42,12 +52,12 @@ function read_csv(event, filename) {
     let events = [];
     for (let el of allTextLines) {
         let split = el.split(',');
-        if(!isNaN(split[0])) {
+        if (!isNaN(split[0])) {
             datapoints.push(parseFloat(split[0]));
         }
 
         if (split.length > 1) {
-            if(!isNaN(split[1])){
+            if (!isNaN(split[1])) {
                 events.push(parseFloat(split[1]))
             }
         }
@@ -60,13 +70,46 @@ function read_csv(event, filename) {
         'datapoints': datapoints,
         'events': events
     };
-    console.log('parsed csv data', data_dict)
+
+    // make_d3_graph(data_dict)
+    data_dict_arr.push(data_dict);
+    return data_dict_arr
 }
 
-
+//  handles errors
 function error_handler(evt) {
     console.log('error');
     if (evt.target.error.name === "NotReadableError") {
         alert("Cannot read file !");
     }
 }
+
+
+// function new_upload(obj) {
+//     console.log('started new upload');
+//
+//     // console.log(obj.id)
+//     console.log(obj.files);
+//     let files = obj.files; // FileList object
+//     let name_list = [];
+//     for (let i = 0, f; f = files[i]; i++) {
+//         let reader = new FileReader();
+//         var extension = f.name.split('.').pop().toLowerCase();//file extension from input file
+//         if(extension==='csv'){
+//             reader.onload = e => {read_csv(e, f.name);};
+//             reader.readAsText(f);
+//         }
+//         else if (extension==='json'){
+//             reader.onload = e => {read_json(e, f.name);}
+//         }
+//         reader.onerror = error_handler;
+//         name_list.push(f.name);
+//     }
+//     let nextSibling = obj.nextElementSibling;
+//     nextSibling.innerText = name_list;
+//     console.log('data dict arr', data_dict_arr)
+//     make_d3_graph(data_dict_arr,name_list)
+// }
+
+
+//csv file handling functions
